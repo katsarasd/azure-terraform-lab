2bf213a63fdfb44af7c3fe8120b6d3c1001339bf
KATSARAS Dimitrios <KatsarasD@unisystems.gr>
2026-09-04 14:15:51 +0300

    Initial terraform configuration

---
+++ AZ-First-Lab/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo-terraform"
+  location = "westeurope"
+}
--- AZ-First-Lab/providers.tf
+++ AZ-First-Lab/providers.tf
@@ -1,12 +1,10 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "~> 4.0"
     }
   }
 }
