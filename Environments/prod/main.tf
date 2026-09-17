8ecc4c4d15239973a2569a034393481edfc72bf2
KATSARAS Dimitrios <KatsarasD@unisystems.gr>
2026-09-17 16:03:12 +0300

    Terraform Azure Prod 10th Deploy

--- Environments/prod/main.tf
+++ Environments/prod/main.tf
@@ -8,21 +8,21 @@ module "rg-con-hub" {
 module "rg-spoke01-net" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
 
-  name     = "rg-spoke01-${var.environment}-${var.location_short}-net-01"
+  name     = "rg-spoke01-${var.environment}-net-${var.location_short}-01"
   location = var.location
 }
 
 module "rg-spoke02-net" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
 
-  name     = "rg-spoke02-${var.environment}-${var.location_short}-net-01"
+  name     = "rg-spoke02-${var.environment}-net-${var.location_short}-net-01"
   location = var.location
 }
 
 module "rg-spoke03-net" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
 
-  name     = "rg-spoke03-${var.environment}-${var.location_short}-net-01"
+  name     = "rg-spoke03-${var.environment}-net-${var.location_short}-net-01"
   location = var.location
 
 }
