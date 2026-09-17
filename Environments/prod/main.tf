9fc94d00a8f782ef0ea65bc9a24ea68a2e32fc19
KATSARAS Dimitrios <KatsarasD@unisystems.gr>
2026-09-17 14:06:50 +0300

    Terraform Azure Prod 7th Deploy

--- Environments/prod/main.tf
+++ Environments/prod/main.tf
@@ -4,3 +4,15 @@ module "resource_group" {
   name     = "rg-con-${var.environment}-${var.location_short}-01"
   location = var.location
 }
+
+module "virtual_network" {
+  source = "Azure/avm-res-network-virtualnetwork/azurerm"
+
+  name                = "vnet-con-${var.environment}-${var.location_short}-01"
+  resource_group_name = module.resource_group.name
+  location            = var.location
+
+  address_space = [
+    "10.10.0.0/16"
+  ]
+}
