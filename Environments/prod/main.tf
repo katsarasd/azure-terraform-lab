6aa00b92d27b0a8e901022549047937b6ea510bb
KATSARAS Dimitrios <KatsarasD@unisystems.gr>
2026-09-17 14:24:19 +0300

    Terraform Azure Prod 8th Deploy

--- Environments/prod/main.tf
+++ Environments/prod/main.tf
@@ -1,17 +1,51 @@
-module "resource_group" {
+module "rg-con-hub" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
 
   name     = "rg-con-${var.environment}-${var.location_short}-01"
   location = var.location
 }
+module "rg-spoke01-net" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
 
-module "virtual_network" {
+  name     = "rg-spoke01-${var.environment}-${var.location_short}-net-01"
+  location = var.location
+}
+
+module "rg-spoke02-net" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+
+  name     = "rg-spoke01-${var.environment}-${var.location_short}-net-01"
+  location = var.location
+}
+module "hub-vnet" {
   source = "Azure/avm-res-network-virtualnetwork/azurerm"
-  parent_id = module.resource_group.resource_id 
+  parent_id = module.rg-con-hub.resource_id 
   name                = "vnet-con-${var.environment}-${var.location_short}-01"
   location            = var.location
 
   address_space = [
-    "10.10.0.0/16"
+    "10.216.0.0/20"
+  ]
+}
+
+module "spoke01-vnet" {
+  source = "Azure/avm-res-network-virtualnetwork/azurerm"
+  parent_id = module.rg-spoke01-net.resource_id 
+  name                = "vnet-spoke01-${var.environment}-${var.location_short}-01"
+  location            = var.location
+
+  address_space = [
+    "10.216.16.0/20"
+  ]
+}
+
+module "spoke02-vnet" {
+  source = "Azure/avm-res-network-virtualnetwork/azurerm"
+  parent_id = module.rg-spoke02-net.resource_id 
+  name                = "vnet-spoke02-${var.environment}-${var.location_short}-01"
+  location            = var.location
+
+  address_space = [
+    "10.216.16.0/20"
   ]
 }
