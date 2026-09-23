c33d35443abae53900f9bf2beef8a46310e89a09
KATSARAS Dimitrios <KatsarasD@unisystems.gr>
2026-09-23 16:07:26 +0300

    Terraform Azure Prod 24th Deploy

--- Environments/prod/outputs.tf
+++ Environments/prod/outputs.tf
@@ -1,21 +1,21 @@
-output "resource_group_names" {
-  value = local.resource_group_names
-}
+#output "resource_group_names" {
+ # value = local.resource_group_names
+#}
 
-output "vnet_names" {
-value = local.vnet_names
-}
+#output "vnet_names" {
+#value = local.vnet_names
+#}
 
-output "resource_group_ids" {
-  value = {
-    for key, rg in module.resource_group :
-    key => rg.resource_id
-  }
-}
+#output "resource_group_ids" {
+ # value = {
+    #for key, rg in module.resource_group :
+   # key => rg.resource_id
+ # }
+#}
 
-output "vnet_ids" {
-  value = {
-    for key, vnet in module.virtual_network :
-    key => vnet.resource_id
-  }
-}
+#output "vnet_ids" {
+ # value = {
+ #   for key, vnet in module.virtual_network :
+  #  key => vnet.resource_id
+ # }
+#}
